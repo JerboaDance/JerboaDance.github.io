dd180d0f34a5c9b0612a6014e9cf47ebfe00f50a
Nell Waliczek <nell@waliczek.dev>
2022-12-11 13:53:57 -0800

    Adds liniting script and fixes existing errors

--- tools/generateSitemap.js
+++ tools/generateSitemap.js
@@ -1,70 +1,79 @@
+/* eslint-disable no-console */
 import { writeFile } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
 import format from 'xml-formatter';
 
 import { shows, companyMembers, highlights } from '../docs/scripts/database.js';
-import { findCurrentShow } from '../docs/scripts/shows.js'
-import { 
+import { findCurrentShow } from '../docs/scripts/shows.js';
+import {
   getShowUrl,
   getCompanyMemberBioUrl,
-  getHighlightBioUrl } from '../docs/scripts/urls.js';
+  getHighlightBioUrl,
+} from '../docs/scripts/urls.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const priorities = {
   urgent: 1.0,
   important: 0.7,
-  nonUrgent: 0.2
-}
+  nonUrgent: 0.2,
+};
 
 // An array with your links
-let links = [
+const links = [
   { url: '', priority: priorities.urgent },
   { url: '/index.html', priority: priorities.urgent },
   { url: '/about.html', priority: priorities.important },
 ];
 
-const {currentShow, currentPerformance} = findCurrentShow(shows);
-Object.keys(shows).forEach(showId => {
+const { currentShow, currentPerformance } = findCurrentShow(shows);
+Object.keys(shows).forEach((showId) => {
   const priority = (currentShow?.id === showId) ? priorities.urgent : priorities.nonUrgent;
-  links.push({ url: getShowUrl(shows[showId]), priority});
+  links.push({ url: getShowUrl(shows[showId]), priority });
 });
 
 const currentCompanyMembers = currentPerformance?.companyMembers;
-Object.keys(companyMembers).forEach(companyMemberId => {
+Object.keys(companyMembers).forEach((companyMemberId) => {
   const companyMember = companyMembers[companyMemberId];
   if (!companyMember.suppressPage) {
-    const isCurrentCompanyMember = !!(currentCompanyMembers?.find(currentCompanyMember => currentCompanyMember.id === companyMember.id));
-    const priority = isCurrentCompanyMember ? priorities.urgent : priorities.nonUrgent;
-    links.push({ url: getCompanyMemberBioUrl(companyMember), priority })
+    const isCurrent = !!(currentCompanyMembers?.find(
+      (currentCompanyMember) => currentCompanyMember.id === companyMember.id,
+    ));
+    const priority = isCurrent ? priorities.urgent : priorities.nonUrgent;
+    links.push({ url: getCompanyMemberBioUrl(companyMember), priority });
   }
 });
 
 const currentHighlights = currentPerformance?.highlights;
-Object.keys(highlights).forEach(highlightId => {
+Object.keys(highlights).forEach((highlightId) => {
   const highlight = highlights[highlightId];
   if (highlight.bio) {
-    const isCurrentHighlight = !!(currentHighlights?.find(currentHighlight => currentHighlight.id === highlight.id));
+    const isCurrent = !!(currentHighlights?.find(
+      (currentHighlight) => currentHighlight.id === highlight.id,
+    ));
     const isCurrentEmcee = currentPerformance?.emcee === highlight;
-    const priority = isCurrentHighlight || isCurrentEmcee ? priorities.urgent : priorities.nonUrgent;
-    links.push({ url: getHighlightBioUrl(highlight), priority })
+    const priority = isCurrent || isCurrentEmcee ? priorities.urgent : priorities.nonUrgent;
+    links.push({ url: getHighlightBioUrl(highlight), priority });
   }
 });
 
 // Create a stream to write to
-const stream = new SitemapStream( { hostname: 'https://jerboadance.com' } )
+const stream = new SitemapStream({ hostname: 'https://jerboadance.com' });
 
 // Return a promise that resolves with your XML string
 streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
   const sitemap = format(data.toString());
   const sitemapFilepath = path.join(__dirname, '../docs/sitemap.xml');
   writeFile(sitemapFilepath, sitemap, (err) => {
-    if (err) return console.log(err);
-    console.log(`Wrote update sitemap to ${sitemapFilepath}`);
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Wrote update sitemap to ${sitemapFilepath}`);
+    }
   });
 });
-
