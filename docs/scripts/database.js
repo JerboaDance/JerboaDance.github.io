ce88e79e9e3178edd72d914ed4a5c71c40c985b8
Gary <garywa@microsoft.com>
2026-01-25 15:37:07 -0800

    update with bios

--- docs/scripts/database.js
+++ docs/scripts/database.js
@@ -754,20 +754,24 @@ const highlights = {
   terrycrane: {
     id: 'terrycrane',
     name: 'Terry Crane',
-    role: 'Guest Performer',
+    role: 'Circus Artist',
     headshot: {
       filename: 'TerryCrane.jpg',
     },
-    bio: `Terry Crane is a guest performer.`,
+    bio: `Terry Crane surrendered to restless urges that led him to walk calmly away with the circus at the tender age of 20. He is the founder of Acrobatic Conundrum, a contemporary circus company based in Seattle. He has performed on 4 continents, in theaters, under the big top, and on the street. Terry is delighted to share his love of rope technique, circus creation, and collaboration via workshops and performances across the globe. As a director, Terry is on a relentless quest to tell true fictions, contradict human isolation, and portray paradox.`,
   },
   oroki: {
     id: 'oroki',
     name: 'Oroki',
-    role: 'Guest Performer',
+    role: 'Circus Artist',
     headshot: {
       filename: 'Oroki.jpg',
     },
-    bio: `Oroki is a guest performer.`,
+    bio: `Oroki is a Seattle-based aerial instructor, performer, and producer, currently teaching at ECTA/SANCA.
+    <p>
+    As a burlesque artist, they have performed in Seattle Vice '76, Moisture Festival, What the Funk Fest, the Seattle Boylesque Festival, ECTA's Mardi Gras, the Seattle Erotic Arts Festival, and Broho Takes Flight.
+    <p>
+    They also produce works such as Alice in Shadowland and A Circus of Steam & Shadows. Coming up they'll be producing a show on the Rice on Repeat: An AAPI Circus at the Seattle Center and 12th Avenue Arts. Details coming through social media, website and newsletter!`,
   },
 };
 
