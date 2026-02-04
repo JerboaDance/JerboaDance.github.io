3daa17afe0a885a96f20889d0ee33257294a134e
Gary <garywa@microsoft.com>
2026-02-04 14:54:30 -0800

    Fix youtube videos

--- docs/scripts/youTube.js
+++ docs/scripts/youTube.js
@@ -2,7 +2,7 @@ function generateYouTubeEmbed(video) {
   const embedElement = document.createElement('div');
   embedElement.setAttribute('class', `youtube youtube-${video.aspectRatio}`);
   embedElement.innerHTML = `
-  <iframe width="784" height="441" src="https://www.youtube.com/embed/${video.id}?list=TLGGBHKwmQE3FvgyOTExMjAyMg" title="Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  <iframe width="784" height="441" src="https://www.youtube.com/embed/${video.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   return embedElement;
 }
 
